1edd12c373b0997ae44c490d9a4695be0f9f6099
byhyakimaro <moderrevolution090217@gmail.com>
2022-09-26 11:14:56 -0300

    save

--- pages/middleware.ts
+++ pages/middleware.ts
@@ -2,11 +2,23 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/about')) {
-    return NextResponse.rewrite(new URL('/about-2', request.url))
-  }
+  // Setting cookies on the response
+  const response = NextResponse.next()
+  response.cookies.set('vercel', 'fast')
+  response.cookies.set('vercel', 'fast', { path: '/test' })
 
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.rewrite(new URL('/dashboard/user', request.url))
-  }
+  // Getting cookies from the request
+  const cookie = request.cookies.get('vercel')
+  console.log(cookie) // => 'fast'
+  const allCookies = request.cookies.entries()
+  console.log(allCookies) // => [{ key: 'vercel', value: 'fast' }]
+  const { value, options } = response.cookies.getWithOptions('vercel')
+  console.log(value) // => 'fast'
+  console.log(options) // => { Path: '/test' }
+
+  // Deleting cookies
+  response.cookies.delete('vercel')
+  response.cookies.clear()
+
+  return response
 }
